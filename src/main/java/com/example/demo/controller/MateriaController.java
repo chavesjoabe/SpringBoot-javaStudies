135fdf1f635dc6df98cd85896b1662573d1f46bd
Joabe Chaves <chavesjoabe1@gmail.com>
2020-12-07 17:46:05 -0300

    adicinado metodos HTTP (delete)

--- src/main/java/com/example/demo/controller/MateriaController.java
+++ src/main/java/com/example/demo/controller/MateriaController.java
@@ -1,17 +1,52 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.MateriaEntity;
+import com.example.demo.repositories.MateriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/materia")
 public class MateriaController {
-    @GetMapping("/")
-    public ResponseEntity<String> helloWorldRest(){
 
-        return ResponseEntity.status(HttpStatus.OK).body("Ola Mundo Rest");
+    @Autowired
+    private MateriaRepository materiaRepository;
+
+    @GetMapping
+    public ResponseEntity<List<MateriaEntity>> listarMaterias(){
+
+        return ResponseEntity.status(HttpStatus.OK).body(materiaRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MateriaEntity> consultaMateria(@PathVariable Long id){
+
+        return ResponseEntity.status(HttpStatus.OK).body(this.materiaRepository.findById(id).get());
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Boolean> cadastrarMateria(@RequestBody MateriaEntity materia){
+        try {
+            this.materiaRepository.save(materia);
+            return ResponseEntity.ok().body(true);
+        }
+        catch (Exception e){
+            return ResponseEntity.ok().body(false);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deletarMateria(@PathVariable Long id){
+        MateriaEntity materia = materiaRepository.findById(id).get();
+
+        materiaRepository.delete(materia);
+
+        return ResponseEntity.ok().body(true);
     }
 }
---
+++ src/main/java/com/example/demo/repositories/MateriaRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entity.MateriaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MateriaRepository extends JpaRepository<MateriaEntity, Long> {
+
+}
